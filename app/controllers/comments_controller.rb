8f3662fd3506b1e8f0f476660f17f0dd093da1b2
nickAJ286 <ajiji3f@gmail.com>
2023-08-15 21:55:02 +0900

    エラー修正（プロフィール、空のコメント投稿）

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,10 @@ class CommentsController < ApplicationController
     if comment.save
         redirect_to "/prototypes/#{comment.prototype.id}"
     else
-       render "prototypes/show"
+        @prototype = comment.prototype
+        @comment = Comment.new
+        @comments = @prototype.comments.includes(:user)
+        render "prototypes/show"
     end
   end
 
